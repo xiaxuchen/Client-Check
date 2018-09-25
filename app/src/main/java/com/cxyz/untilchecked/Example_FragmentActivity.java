5bb64a4f09e2784d23e1ce238c8b6cc7b331f52b
xiaxuchen <37445028+xiaxuchen@users.noreply.github.com>
2018-09-25 13:31:59 +0800

    在App中： 写了一些使用BaseFragment和FragmentActivity的例子 写了使用AdapterBase和ViewHolder的例子

---
+++ app/src/main/java/com/cxyz/untilchecked/Example_FragmentActivity.java
@@ -0,0 +1,54 @@
+package com.cxyz.untilchecked;
+
+import com.cxyz.commons.IPresenter.IBasePresenter;
+import com.cxyz.commons.activity.FragmentActivity;
+import com.cxyz.commons.fragment.BaseFragment;
+
+/**
+ * Created by 夏旭晨 on 2018/9/25.
+ */
+
+public class Example_FragmentActivity extends FragmentActivity{
+    @Override
+    public int getFragmentContentId() {
+        return R.id.content;
+    }
+
+    @Override
+    protected BaseFragment getFirstFragment() {
+        return Example_Fragmment.newInstance();
+    }
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_example_layout;
+    }
+
+    @Override
+    public void initView() {
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setEvent() {
+    }
+
+    @Override
+    protected IBasePresenter createIPresenter() {
+        return null;
+    }
+
+    @Override
+    public void showLoadingView() {
+
+    }
+
+    @Override
+    public void hideLoadingView() {
+
+    }
+}
