7cba82bf5f7e168882608bb2b9a141e3c6d1d899
q956822950 <39487187+q956822950@users.noreply.github.com>
2018-10-13 16:36:05 +0800

    10.13 我的考勤layout

---
+++ mine/src/main/Java/com/cxyz/mine/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.cxyz.mine.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cxyz.mine.R;
+
+/**
+ * Created by Administrator on 2018/10/13.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
---
+++ mine/src/main/Java/com/cxyz/mine/activity/MyCheckActivity.java
@@ -0,0 +1,177 @@
+package com.cxyz.mine.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cxyz.mine.R;
+import com.qmuiteam.qmui.widget.QMUIProgressBar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyCheckActivity extends AppCompatActivity {
+    private ImageView iv_absent;
+    private Bitmap bitmap;
+    private QMUIProgressBar qmuiProgressBar;
+    private MyExpandableListViewAdapter adapter;
+    private ExpandableListView el_checksituation;
+    private Map<String, List<String>> dataset = new HashMap<>();
+    private String[] parentList = new String[]{"first", "second", "third"};
+    private List<String> childrenList1 = new ArrayList<>();
+    private List<String> childrenList2 = new ArrayList<>();
+    private List<String> childrenList3 = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mycheck_activity);
+        iv_absent=findViewById(R.id.iv_absent);
+        el_checksituation = (ExpandableListView) findViewById(R.id.el_checksituation);
+        qmuiProgressBar = findViewById(R.id.qmuiProgressBar);
+        initialData();
+        adapter = new MyExpandableListViewAdapter();
+        el_checksituation.setAdapter(adapter);
+    }
+
+    private void initialData() {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dotyellow);
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList2.add(parentList[1] + "-" + "first");
+        childrenList2.add(parentList[1] + "-" + "second");
+        childrenList2.add(parentList[1] + "-" + "third");
+        childrenList3.add(parentList[2] + "-" + "first");
+        childrenList3.add(parentList[2] + "-" + "second");
+        childrenList3.add(parentList[2] + "-" + "third");
+        dataset.put(parentList[0], childrenList1);
+        dataset.put(parentList[1], childrenList2);
+        dataset.put(parentList[2], childrenList3);
+        qmuiProgressBar.setMaxValue(100);
+        qmuiProgressBar.setProgress(100);
+        qmuiProgressBar.setQMUIProgressBarTextGenerator(new QMUIProgressBar.QMUIProgressBarTextGenerator() {
+            @Override
+            public String generateText(QMUIProgressBar progressBar, int value, int maxValue) {
+
+                return value + "%";
+            }
+        });
+
+        iv_absent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private class MyExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+        //  获得某个父项的某个子项
+        @Override
+        public Object getChild(int parentPos, int childPos) {
+            return dataset.get(parentList[parentPos]).get(childPos);
+        }
+
+        //  获得父项的数量
+        @Override
+        public int getGroupCount() {
+            return dataset.size();
+        }
+
+        //  获得某个父项的子项数目
+        @Override
+        public int getChildrenCount(int parentPos) {
+            return dataset.get(parentList[parentPos]).size();
+        }
+
+        //  获得某个父项
+        @Override
+        public Object getGroup(int parentPos) {
+            return dataset.get(parentList[parentPos]);
+        }
+
+        //  获得某个父项的id
+        @Override
+        public long getGroupId(int parentPos) {
+            return parentPos;
+        }
+
+        //  获得某个父项的某个子项的id
+        @Override
+        public long getChildId(int parentPos, int childPos) {
+            return childPos;
+        }
+
+        //  按函数的名字来理解应该是是否具有稳定的id，这个方法目前一直都是返回false，没有去改动过
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int parentPos, boolean b, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) MyCheckActivity
+                        .this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.parent_item, null);
+            }
+            view.setTag(R.layout.parent_item, parentPos);
+            view.setTag(R.layout.child_item, -1);
+            TextView text = (TextView) view.findViewById(R.id.parent_title);
+            ImageView imageView = view.findViewById(R.id.parent_image);
+
+            text.setText(parentList[parentPos]);
+            return view;
+        }
+
+        public View getChildView(int parentPos, int childPos, boolean b, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) MyCheckActivity
+                        .this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.child_item, null);
+            }
+            view.setTag(R.layout.parent_item, parentPos);
+            view.setTag(R.layout.child_item, childPos);
+            TextView text = (TextView) view.findViewById(R.id.child_title);
+            ImageView imageView = view.findViewById(R.id.child_image);
+            imageView.setImageBitmap(bitmap);
+            text.setText(dataset.get(parentList[parentPos]).get(childPos));
+            text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MyCheckActivity.this, "点到了内置的textview", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return view;
+        }
+
+        //  获得父项显示的view
+
+
+        //  获得子项显示的view
+
+
+        //  子项是否可选中，如果需要设置子项的点击事件，需要返回true
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return false;
+        }
+    }
+}
