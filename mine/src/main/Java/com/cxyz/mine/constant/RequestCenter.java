ea066b9a2fe858d3db14412747fcfa1215055a9a
q956822950 <39487187+q956822950@users.noreply.github.com>
2018-10-24 14:14:43 +0800

    版本更新  自动进入安装程序 。小米等系统有问题

---
+++ mine/src/main/Java/com/cxyz/mine/constant/RequestCenter.java
@@ -0,0 +1,24 @@
+package com.cxyz.mine.constant;
+
+import android.accounts.NetworkErrorException;
+
+import com.cxyz.commons.utils.HttpUtil.CommonOkHttpClient;
+import com.cxyz.commons.utils.HttpUtil.listener.DisposeDataHandler;
+import com.cxyz.commons.utils.HttpUtil.listener.DisposeDataListener;
+import com.cxyz.commons.utils.HttpUtil.request.RequestParams;
+
+/**
+ * Created by 夏旭晨 on 2018/10/14.
+ */
+
+public class RequestCenter {
+
+    public static void login(RequestParams params, DisposeDataListener listener){
+        try {
+            CommonOkHttpClient.post(NetWorkConstant.LOGIN_URL,params,new DisposeDataHandler(listener));
+        } catch (NetworkErrorException e) {
+            e.printStackTrace();
+            listener.onFailure("网络状态异常");
+        }
+    }
+}
