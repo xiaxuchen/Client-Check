faed3df1fda3d15347d94bea0577ce2e8ce2915f
q956822950 <39487187+q956822950@users.noreply.github.com>
2018-12-28 18:10:36 +0800

    Merge branches 'supery' and 'test-y' of https://github.com/xiaxuchen/Client-Check into supery
    # Conflicts:
    #	homepage/src/main/java/com/cxyz/homepage/fragment/HomeFragment.java
    #	logiccommons/src/main/java/com/cxyz/logiccommons/constant/Constant.java
    
    //12.28  15：14
    //暂时完成考勤统计部分功能（除班级ID）

--- mine/src/main/java/com/cxyz/mine/adapter/AppoitmentAdapter.java
+++ mine/src/main/java/com/cxyz/mine/adapter/AppoitmentAdapter.java
@@ -1,6 +1,7 @@
 package com.cxyz.mine.adapter;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,50 +52,63 @@ public class AppoitmentAdapter extends BaseAdapter {
         //获得学生对象
         AppointmentInfoBean studentinfo = mData.get(position);
         //获得自定义布局中每一个控件的对象。
-        QMUIRadiusImageView image = (QMUIRadiusImageView) viewStudent.findViewById(R.id.iv_appointmentstu_image);
-        final LinearLayout ll_appointmentvisible_info=viewStudent.findViewById(R.id.ll_appointmentvisible_info);
-        final Button  button=viewStudent.findViewById(R.id.bt_appointmentstu_check);
-        TextView name = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_name);
-        TextView code = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_code);
-        TextView info = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_info);
-        TextView time = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_time);
-        TextView reason = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_reason);
-        TextView moreinfo = (TextView) viewStudent.findViewById(R.id.tv_appointmentstu_moreinfo);
+        QMUIRadiusImageView image = (QMUIRadiusImageView) viewStudent.findViewById(R.id.iv_appoint_image);
+        final LinearLayout ll_appointmentvisible_info=viewStudent.findViewById(R.id.ll_appoint_info);
+        final Button  bt_appoint_check=viewStudent.findViewById(R.id.bt_appoint_check);
+        TextView name = (TextView) viewStudent.findViewById(R.id.tv_appoint_name);
+        TextView code = (TextView) viewStudent.findViewById(R.id.tv_appoint_code);
+        TextView info = (TextView) viewStudent.findViewById(R.id.tv_appoint_info);
+        TextView time = (TextView) viewStudent.findViewById(R.id.tv_appoint_time);
+        TextView moreinfo = (TextView) viewStudent.findViewById(R.id.tv_appoint_moreinfo);
+        final LinearLayout ll_appoint_message=viewStudent.findViewById(R.id.ll_appoint_message);
+        final Button bt_appoint_reject=viewStudent.findViewById(R.id.bt_appoint_reject);
         info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if(click==true){
                     click=false;
+                    ll_appoint_message.setVisibility(View.GONE);
                 ll_appointmentvisible_info.setVisibility(View.VISIBLE);
+
 
             }else if(click==false){
                     click=true;
                     ll_appointmentvisible_info.setVisibility(View.GONE);
-
+                    ll_appoint_message.setVisibility(View.VISIBLE);
             }
             }
         });
         //老师审核
-        button.setOnClickListener(new View.OnClickListener() {
+        bt_appoint_check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button.setClickable(false);
-                button.setText("已审核");
-                button.setBackgroundColor(R.color.qmui_config_color_blue);
+                bt_appoint_check.setClickable(false);
+                bt_appoint_reject.setVisibility(View.GONE);
+                bt_appoint_check.setBackgroundResource(R.drawable.main_shapegray);
+                bt_appoint_check.setText("已审核");
+            }
+        });
+        bt_appoint_reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt_appoint_reject.setClickable(false);
+                bt_appoint_check.setVisibility(View.GONE);
+                bt_appoint_reject.setBackgroundResource(R.drawable.main_shapegray);
+                bt_appoint_reject.setText("已拒绝");
             }
         });
         image.setCircle(true);
         image.setCornerRadius(100);
         image.setOval(false);//是否椭圆
-        image.setBorderWidth(10);//设置边缘宽度
+        image.setBorderWidth(5);//设置边缘宽度
+        image.setBorderColor(Color.CYAN);
 
         //将数据一一添加到自定义的布局中。
         image.setImageResource(studentinfo.getImage());
         name.setText(studentinfo.getName());
         code.setText(studentinfo.getCode());
         time.setText(studentinfo.getTime());
-        reason.setText(studentinfo.getReason());
         moreinfo.setText(studentinfo.getMoreinfo());
         return viewStudent;
     }
